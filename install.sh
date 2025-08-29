d7833235f8e05c5edada88018cb69c69ef64ed7c
mark <markoc1120@gmail.com>
2025-08-29 21:24:23 +0200

    feat: initial migration to sway (wip)

--- install.sh
+++ install.sh
@@ -8,12 +8,18 @@
 # sudo pacman -S --noconfirm --needed gcc make git ripgrep fd unzip neovim
 ln -sf "$DOTFILES/nvim/" "$XDG_CONFIG_HOME/"
 
-#######
-# X11 #
-#######
+###########
+# ghostty #
+###########
 
-rm -rf "$XDG_CONFIG_HOME/X11"
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+rm -rf "$XDG_CONFIG_HOME/ghostty"
+ln -sf "$DOTFILES/ghostty/" "$XDG_CONFIG_HOME/"
+
+########
+# sway #
+########
+
+ln -sf "$DOTFILES/sway/" "$XDG_CONFIG_HOME/"
 
 ######
 # i3 #
@@ -29,6 +35,7 @@ ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
 ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zprofile" "$XDG_CONFIG_HOME/zsh/.zprofile"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
@@ -41,12 +48,12 @@ ln -sf "$DOTFILES/zsh/scripts.sh" "$XDG_CONFIG_HOME/zsh"
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
-#################
-# Notifications #
-#################
-
-mkdir -p "$XDG_CONFIG_HOME/dunst"
-ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+# #################
+# # Notifications #
+# #################
+#
+# mkdir -p "$XDG_CONFIG_HOME/dunst"
+# ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 
 #######
 # Git #
@@ -69,8 +76,8 @@ ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 ln -s "$DOTFILES/tmuxp" "$XDG_CONFIG_HOME"
 
 #############
-# redshifts #
+# gammastep #
 #############
 
-mkdir -p "$XDG_CONFIG_HOME/redshift"
-ln -sf "$DOTFILES/redshift/redshift.conf" "$XDG_CONFIG_HOME/redshift/redshift.conf"
+mkdir -p "$XDG_CONFIG_HOME/gammastep"
+ln -sf "$DOTFILES/gammastep/config" "$XDG_CONFIG_HOME/gammastep/config"
